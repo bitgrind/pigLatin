e4731f1a76896ec3c2bd45bbe1e9dc920a389f68
Keith Stedman <keithstedman@gmail.com>
2017-02-22 15:37:38 -0800

    Pig latin translator working for I, THis, Chair, Squeal

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,44 @@ $(function(){
 
   function pigLatinIZE(string, index) {
     //console.log("Latinize, st: "+string + ", index: " + index);
-    var firstConsonants = "";
-    var sliceLength = string.length-index;
-    console.log(sliceLength);
-    var remainderWord = string.slice(index,(sliceLength+index));
-    for(j=0;j<index;j++){
-      firstConsonants += string.charAt(j);
-      console.log("pigLatinIZE: "+ string.charAt(j));
-    }
 
-    console.log("Pig Laten String: " + firstConsonants);
-    console.log("Remainder of SLICE: " + remainderWord);
-    $("#output h2").text(remainderWord+firstConsonants+"ay");
+    if(string.charAt(index) == "u") {
+      console.log("PIG LATINIZE IF");
+      var firstConsonants = string.slice(0,index+1);
+      var remainderWord = string.slice(index+1,string.length);
+      $("#output h2").text(remainderWord+firstConsonants+"ay");
+    }else{
+      console.log("PIG LATINIZE ELSE");
+      var firstConsonants = string.slice(0,index);
+      var remainderWord = string.slice(index,string.length);
+      $("#output h2").text(remainderWord+firstConsonants+"ay");
+    // for(j=0;j<index;j++){
+    //   console.log("string index " + string.charAt(index));
+    //   firstConsonants += string.charAt(j);
+      // if(string[index] == "u") {
+      //   console.log(firstConsonants);
+      //   console.log("found that U!!!");
+      // }
+    //}
+    }
   }
 
   function isVowel(string) {
     var vowel = ["a", "e", "i", "o", "u"];
+    var k = 0;
+    var index = 0;
     console.log("vowel check: " + string);
-    $(vowel).each(function(){
+    $(string).each(function(){
       console.log("vowel to compare: " + this );
-      for(i=0; i < string.length; i++){
-        if(this == string.charAt(i)){
-          console.log("WE FOUND A VOWEL!");
-          pigLatinIZE(string, i);
-          break;
+      for(i=0; i < string.length-1; i++){
+        console.log("comparing string: " + vowel[i]);
+        if((this == vowel[i]) && (k == 0)){
+          console.log("WE FOUND A VOWEL! " + this + ", index found at:"+ index);
+          k++;
+          pigLatinIZE(string, index);
         }
       }
+      index++;
     });
   }
 
