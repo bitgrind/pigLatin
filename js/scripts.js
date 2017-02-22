cf2f4e9c3bc066aad5ad6b5aed13c49a05d3bf2b
Keith Stedman <keithstedman@gmail.com>
2017-02-22 14:43:23 -0800

    working out logic, got most basics down refining all options

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
 $(function(){
 
-  function isVowel() {
-    
+  function pigLatinIZE(string, index) {
+    //console.log("Latinize, st: "+string + ", index: " + index);
+    var firstConsonants = "";
+    var sliceLength = string.length-index;
+    console.log(sliceLength);
+    var remainderWord = string.slice(index,(sliceLength+index));
+    for(j=0;j<index;j++){
+      firstConsonants += string.charAt(j);
+      console.log("pigLatinIZE: "+ string.charAt(j));
+    }
+
+    console.log("Pig Laten String: " + firstConsonants);
+    console.log("Remainder of SLICE: " + remainderWord);
+    $("#output h2").text(remainderWord+firstConsonants+"ay");
+  }
+
+  function isVowel(string) {
+    var vowel = ["a", "e", "i", "o", "u"];
+    console.log("vowel check: " + string);
+    $(vowel).each(function(){
+      console.log("vowel to compare: " + this );
+      for(i=0; i < string.length; i++){
+        if(this == string.charAt(i)){
+          console.log("WE FOUND A VOWEL!");
+          pigLatinIZE(string, i);
+          break;
+        }
+      }
+    });
   }
 
   //business logic
   function pigLatin(sentence){
-
-    console.log(sentence.length);
     var arraySentence = sentence.split(" ");
-    console.log(arraySentence + ", " + arraySentence.length);
     $(arraySentence).each(function(){
-      console.log($(this));
-
+      isVowel(this);
     });
     return sentence;
   }
@@ -21,7 +44,6 @@ $(function(){
   $("#pigLatin").submit(function(){
     event.preventDefault();
     var sentence = $("#pigLatin input").val();
-    var pigLatinOutput = pigLatin(sentence);
-    $("#output h2").text(pigLatinOutput);
+    pigLatin(sentence);
   });
 });
